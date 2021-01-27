ce642cd4e2dbf366e7d1c46466016353246c8547
Tantely Rabefarihy <tantelyrab@gmail.com>
2021-01-27 12:15:08 -0500

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 const SingleCocktail = () => {
   return (
     <div>
-      <h2>single cocktail page </h2>
+      <h2 style={{ fontWeight: 100 }}>single cocktail page </h2>
     </div>
   );
 };
