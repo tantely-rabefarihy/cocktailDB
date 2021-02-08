8fdf6124f33f6a1aa3354e22fb0adf4ce34f4cb5
Tantely Rabefarihy <tantelyrab@gmail.com>
2021-02-08 14:52:21 -0500

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,9 @@ const Home = () => {
     <main>
       <SearchForm />
       <CocktailList />
+      <div>
+      <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      </div>
     </main>
   );
 };
